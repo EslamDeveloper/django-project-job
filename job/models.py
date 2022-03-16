bf19fb5f4d349bcc44ac87377b40f1543e1a02b3
EslamDeveloper <em59427@email.com>
2022-03-16 21:56:47 +0200

    add basic 4 apps in our project

--- job/models.py
+++ job/models.py
@@ -1,3 +1,16 @@
+from operator import mod
+from random import choices
+from turtle import title
 from django.db import models
 
 # Create your models here.
+JOB_TYPE =( 
+    ('Full Time','Full Time'),
+    ('Part Time','Part Time'),
+)
+
+class job(models.Model): #table
+    title = models.CharField(max_length=100) #coumn
+
+    #location.........
+    job_type = models.CharField(max_length=15, choices = JOB_TYPE)
